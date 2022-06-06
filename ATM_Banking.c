c6f0243039a2e5b9ad487f616bde1efb085286bd
Emmanuel Peter <peterojonilemi@gmail.com>
2022-06-06 15:46:07 +0100

    Fixed the getch() implicit error by adding the conio.h header file

--- ATM_Banking.c
+++ ATM_Banking.c
@@ -21,7 +21,7 @@ int main()
     printf(" <<3>> Igala   <<>>\n\n");
     printf("->Input your number here: ");
     scanf("%d", &opt);
-
+    fgetc(stdin);
     switch (opt)
     {
     case 1:
@@ -31,19 +31,47 @@ int main()
         system("cls");
         system("color 0b");
         printf("\tYou're welcome");
-        take_input(userp, 'a');
         take_password(userp, 1);
         fpoint = fopen("Acct.dat", "r");
         if (fpoint == NULL)
         {
-            fpointrintf(stderr, "Error opening file for reading");
+            fprintf(stderr, "\nError opening file for reading");
             exit(1);
         }
-        // printf("\nYour password is %s\n\n", info.pin);
-        while (fread(&info, sizeof(person), 1, fpoint))
-        
+        while (fread(&user, sizeof(person), 1, fpoint))
+        {
+            if (!compare_str(userp->pin, user.pin))
+            {
+                system("cls");
+                system("color 3A");
+                printf("\n\t\t\t\t\t\t\t\t\nWelcome %s", user.fname);
+                printf("\t\t\tSelect the transaction you want to perform:\n");
+                printf(" <<1>> Withdrawal <<>>\n");
+                printf(" <<2>> Deposit <<>>\n");
+                printf(" <<3>> Balance Enquiries <<>>\n");
+                printf(" <<4>> Transfer <<>>\n");
+                printf(" <<5>> Top-Up Services <<>>\n\n");
+                printf(" <<->> Input your choice here: ");
+                scanf("%d", &opt);
+                switch (opt)
+                {
+                case 1:
+                    system("cls");
+                    system("color 0b");
+                    printf("Kindly select the denomination you want to make your withdrawal: ");
+                    printf("\n <<1>> 500 <<>>");
+                    printf("\t\t\t\t\t\t\t <<2>> 1000 <<>>\n");
+                    printf(" <<3>> 2000 <<>>");
+                    printf("\t\t\t\t\t\t <<4>> 5000 <<>>\n");
+                    // printf(" <<5>> Top-Up Services <<>>\n\n");
+                    break;
 
-        printf("\tYou're welcome");
+                default:
+                    break;
+                }
+            }
+        }
+        fclose(fpoint);
         break;
     case 3:
         printf("\tMegwa oo");
