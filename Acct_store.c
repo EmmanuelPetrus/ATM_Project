8a8a1d44bf464bf0636ba940188c610278220f36
Emmanuel Peter <peterojonilemi@gmail.com>
2022-06-29 10:04:10 +0100

    Error discovered in the binary file as the read and write projects values that aren't stored

--- Acct_store.c
+++ Acct_store.c
@@ -76,6 +76,8 @@ int main(void)
         }
         while (fread(&info_storex, sizeof(person), 1, fp))
         {
+            printf("\n%d\n",ftell(fp));
+            printf("\n%d",info_storex.acct_num);
             if (!compare_str(info_storex.acct_num, info.acct_num))
             {
 
